f79624145fb7e59f6e623bd5edcf5fc6431e7263
Hania <haniagit08@gmail.com>
2018-08-18 19:04:56 +0200

    Book model created

---
+++ src/main/java/com/hania/library/model/Book.java
@@ -0,0 +1,41 @@
+package com.hania.library.model;
+
+public class Book {
+
+    private String author;
+    private String title;
+    private int print_year;
+    private String genre;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrint_year() {
+        return print_year;
+    }
+
+    public void setPrint_year(int print_year) {
+        this.print_year = print_year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
